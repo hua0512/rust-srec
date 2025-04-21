216fd98ba6b9d2eeeebbb53c7bb4f4fc0a65d23e
hua0512 <whua0512@gmail.com>
2025-04-21 20:02:44 +0200

    feat(siphon): support hls downloads

--- crates/siphon/src/lib.rs
+++ crates/siphon/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod downloader;
 pub mod error;
-pub mod flv_downloader;
+pub mod flv;
+pub mod hls;
 pub mod proxy;
 mod utils;
 
 pub use downloader::DownloaderConfig;
 pub use error::DownloadError;
-pub use flv_downloader::{FlvDownloader, RawByteStream};
+pub use flv::flv_downloader::{FlvDownloader, RawByteStream};
+pub use hls::hls_downloader::HlsDownloader;
 pub use proxy::{ProxyAuth, ProxyConfig, ProxyType};
